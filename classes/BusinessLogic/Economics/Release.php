efcacc141b86e790f3baa4aa002171f341247d12
Christian Fürst <fuerscht@gmail.com>
2016-08-06 16:31:23 +0200

    NEUR-1 Limit Shown Indicators In Release View

--- classes/BusinessLogic/Economics/Release.php
+++ classes/BusinessLogic/Economics/Release.php
@@ -9,6 +9,10 @@ class Release extends Release_Generated {
 		//first get Release in non eager mode from parent::getById
 		//second get Indicators from parent::getByNamedFieldValues function
 		
+		$result = parent::getById($ontologyClassName, $id, $eager, $cascades);
+		
+		$result->Indicators = $this->getByNamedFieldValues("Indicator", array("releaseID"), array($id), false, null, false, true, null, "popularity DESC", 10);
+		
 		return $result;
 	}
 	
