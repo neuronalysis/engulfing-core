9f4e341a07511fe4e30707bc036827fec06d5431
Christian Fürst <fuerscht@gmail.com>
2018-02-10 12:59:22 +0100

    KOKOS #2 related modal extensions confirmatio callback support

--- js/views/components/modal.js
+++ js/views/components/modal.js
@@ -1,7 +1,26 @@
 var ModalView = BaseView.extend({
 	initialize : function(options) {
+		this.template = _.template(tpl.get('components/modal'));
+		
+		this.parent = options.parent;
+		this.bodyText = options.bodyText;
+		if (options.confirmationCallback) this.confirmationCallback = options.confirmationCallback.confirmationCallback;
+		
 	},
 	events : {
+		"click #btn_confirm" : "confirmQuestion",
+	},
+	confirmQuestion : function() {
+		this.confirmationCallback(this.parent);
+		
+		this.$('#contextMenu').modal('toggle');
+	},
+	render : function() {
+		var data = {"bodyText": this.bodyText};
+
+		this.$el.html(this.template(data));
+		
+		return this;
 	}
 });
 		
