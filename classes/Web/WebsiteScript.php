52060e889da47c2a6cb4f95859a8e4ff365de296
Christian Fürst <fuerscht@gmail.com>
2019-04-26 12:18:18 +0200

    exclude analytics from parent class

--- classes/Web/WebsiteScript.php
+++ classes/Web/WebsiteScript.php
@@ -505,22 +505,6 @@ var referrer = document.referrer;
 	
 		return $html;
 	}
-	function renderHTMLScripts_Analytics() {
-		$html = "";
-		
-		if (!$this->isLocalRequest()) {
-			$html = "
-		<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-			
-  ga('create', 'UA-58793893-1', 'auto');
-  ga('send', 'pageview');</script>";
-		}
-		
-		return $html;
-	}
 	function renderHTMLScripts_Controller($scope) {
 		$scopeDepth = $this->getScopeDepth();
 		$objectName = $this->getRefererScopeName($scope);
